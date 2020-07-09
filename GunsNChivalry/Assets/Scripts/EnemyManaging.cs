83c1556c17e32b38dbf181e0ee263eaf3c7e5968
Shawrex <38359132+Shawrex@users.noreply.github.com>
2020-07-09 20:47:13 +0200

    Knight Targeting + First Enemies Things

---
+++ GunsNChivalry/Assets/Scripts/EnemyManaging.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManaging : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public static EnemyManaging instance;
+
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+    }
+}
---
+++ GunsNChivalry/Assets/Scripts/RotationTargeting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTargeting : MonoBehaviour
+{
+    private string targeting;
+    private GameObject target;
+
+    private void Start()
+    {
+        targeting = "first";
+    }
+
+    void LateUpdate()
+    {
+        GameObject[] enemies = EnemyManaging.instance.enemies;
+
+        if (targeting == "first")
+        {
+            target = enemies[0];
+        }
+
+        transform.up = target.transform.position;
+    }
+}
