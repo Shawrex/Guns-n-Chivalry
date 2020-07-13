a6f905085344d3db13fab76f1e3f2bda69603dc1
Shawrex <38359132+Shawrex@users.noreply.github.com>
2020-07-13 12:53:25 +0200

    Dungeon [FINISHED]

--- GunsNChivalry/Assets/Scripts/RoomSpawner.cs
+++ GunsNChivalry/Assets/Scripts/RoomSpawner.cs
@@ -5,47 +5,76 @@ using UnityEngine;
 public class RoomSpawner : MonoBehaviour
 {
     private int placement = 0; //ex : 0 = U -> Need D Door
-    private RoomsTemplates rt;
-    private bool spawned;
+    private RoomsTemplates rt; //All the rooms templates
+    private static int rooms; //The number of rooms spawned ( to keep an -okay- dungeon )
+    private GameObject r; //The room that i'll spawn;
 
     private void Start()
     {
         Vector3 pos = transform.localPosition;
 
         if (pos.y > 0)
-            placement = 0; //U
+            placement = 0; //I'm a Up point
         else if (pos.y < 0)
-            placement = 1; //D
+            placement = 1; //Down point
         else if (pos.x < 0)
-            placement = 2; //L
+            placement = 2; //Left
         else if (pos.x > 0)
-            placement = 3; //R
+            placement = 3; //Right 
 
         Invoke("Spawn", 0.1f);
     }
 
     void Spawn()
     {
-        if (!spawned)
+        rt = RoomsTemplates.instance; //Reference to the only one rooms templates script ( a sort of database )
+
+        if (placement == 0)
         {
-            rt = RoomsTemplates.instance;
+            //Need a room with a Down door
 
-            if (placement == 0)
-                Instantiate(rt.dRooms[Random.Range(0, rt.dRooms.Length)], transform.position, Quaternion.identity); //Spawn a D
-            else if (placement == 1)
-                Instantiate(rt.uRooms[Random.Range(0, rt.uRooms.Length)], transform.position, Quaternion.identity); //Spawn a U
-            else if (placement == 2)
-                Instantiate(rt.rRooms[Random.Range(0, rt.rRooms.Length)], transform.position, Quaternion.identity); //Spawn a R
-            else if (placement == 3)
-                Instantiate(rt.lRooms[Random.Range(0, rt.lRooms.Length)], transform.position, Quaternion.identity); //Spawn a L
+            if (rooms < 20) //If there isn't that much rooms , i can sawn randomly
+                r = Instantiate(rt.dRooms[Random.Range(0, rt.dRooms.Length)], transform.position, Quaternion.identity); //randomly
+            else
+                r = Instantiate(rt.dRoom, transform.position, Quaternion.identity); //No more exits
 
-            spawned = true;
         }
+        else if (placement == 1)
+        {
+            //Need a room with a Up door
+
+            if (rooms < 20)
+                r = Instantiate(rt.uRooms[Random.Range(0, rt.uRooms.Length)], transform.position, Quaternion.identity); //randomly
+            else
+                r = Instantiate(rt.uRoom, transform.position, Quaternion.identity); //No more exits
+        }
+        else if (placement == 2)
+        {
+            //Need a room with a Right door
+
+            if (rooms < 20)
+                r = Instantiate(rt.rRooms[Random.Range(0, rt.rRooms.Length)], transform.position, Quaternion.identity); //randomly
+            else
+                r = Instantiate(rt.rRoom, transform.position, Quaternion.identity); //No more exits
+        }
+        else if (placement == 3)
+        {
+            //Need a room with a Left door
+
+            if (rooms < 20)
+                r = Instantiate(rt.lRooms[Random.Range(0, rt.lRooms.Length)], transform.position, Quaternion.identity); //randomly
+            else
+                r = Instantiate(rt.lRoom, transform.position, Quaternion.identity); //No more exits
+        }
+
+        rooms++;
+
+        rt.roomsSpawned.Add(r);
     }
 
     private void OnTriggerEnter2D(Collider2D c)
     {
         if (c.CompareTag("SpawnPoints"))
-            Destroy(gameObject);
+            Destroy(gameObject); //If I encounter another point or a room , destroy myself
     }
 }
--- GunsNChivalry/Assets/Scripts/RoomsTemplates.cs
+++ GunsNChivalry/Assets/Scripts/RoomsTemplates.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class RoomsTemplates : MonoBehaviour
 {
-    public GameObject[] uRooms;
-    public GameObject[] dRooms;
-    public GameObject[] lRooms;
-    public GameObject[] rRooms;
+    public GameObject[] uRooms, dRooms, lRooms,  rRooms; //Rooms with Up , Down , Left , Right doors to spawn;
+    public GameObject uRoom, dRoom, lRoom, rRoom; //Rooms used to stop spawning more and close the dungeon;
 
-    public static RoomsTemplates instance;
+    [Header("Do not put anything here")]
+    public List<GameObject> roomsSpawned; //List of rooms spawned
+
+    public static RoomsTemplates instance; //This
 
     private void Start()
     {
         if (instance == null)
-            instance = this;
+            instance = this; //Set it to the instance to get refered easily
+
+        Invoke("Exit", 2f); //2 seconds after start I call Exit()
+    }
+
+    private void Exit()
+    {
+        roomsSpawned[roomsSpawned.Count - 1].GetComponent<SpriteRenderer>().color = Color.red; //Turn the last room spawned to red ( boss )
     }
 }
